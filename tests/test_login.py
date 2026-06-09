865349534fbb5c466ebbc01e5733dadebf68f691
missy1112 <misslanzunza@outlook.com>
2026-06-09 22:46:38 +0200

    Add logout test and improve waits

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -14,7 +16,9 @@ def test_valid_login():
         driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
         driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 
-        success_message = driver.find_element(By.ID, "flash").text
+        success_message = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.ID, "flash"))
+        ).text
 
         assert "You logged into a secure area!" in success_message
 
@@ -32,9 +36,37 @@ def test_invalid_login():
         driver.find_element(By.ID, "password").send_keys("wrongpassword")
         driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 
-        error_message = driver.find_element(By.ID, "flash").text
+        error_message = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.ID, "flash"))
+        ).text
 
         assert "Your username is invalid!" in error_message
+
+    finally:
+        driver.quit()
+
+
+def test_logout_after_successful_login():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    try:
+        driver.get("https://the-internet.herokuapp.com/login")
+
+        driver.find_element(By.ID, "username").send_keys("tomsmith")
+        driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
+        driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+        WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.ID, "flash"))
+        )
+
+        driver.find_element(By.CSS_SELECTOR, "a[href='/logout']").click()
+
+        logout_message = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.ID, "flash"))
+        ).text
+
+        assert "You logged out of the secure area!" in logout_message
 
     finally:
         driver.quit()
