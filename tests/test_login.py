4a30f3869d08ee98568ff0505caa05696ba12285
missy1112 <misslanzunza@outlook.com>
2026-06-09 22:39:55 +0200

    Add screenshot for two passing login tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -20,3 +20,21 @@ def test_valid_login():
 
     finally:
         driver.quit()
+
+
+def test_invalid_login():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    try:
+        driver.get("https://the-internet.herokuapp.com/login")
+
+        driver.find_element(By.ID, "username").send_keys("wronguser")
+        driver.find_element(By.ID, "password").send_keys("wrongpassword")
+        driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+        error_message = driver.find_element(By.ID, "flash").text
+
+        assert "Your username is invalid!" in error_message
+
+    finally:
+        driver.quit()
